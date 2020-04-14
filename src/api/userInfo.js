963b1b2e9c4419e5482f958b0ec17d4f26db1617
zhuantou <953685809@qq.com>
2020-04-14 20:56:33 +0800

    重建

--- src/api/userInfo.js
+++
@@ -1,9 +0,0 @@
-import service from '@/utils/request'
-
-// 检测手机号是否注册
-export function testPhoneRegister() {
-  return service({
-    url: `/api/cellphone/existence/check?phone=13870083309`,
-    method: 'post'
-  })
-}
